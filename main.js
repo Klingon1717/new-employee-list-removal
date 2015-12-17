6427baac87041ca286c7406ce71823e295f050df
Whitney Murray <wnksmith92@yahoo.com>
2015-12-17 02:55:47 -0600

    trying to use a for loop

--- main.js
+++ main.js
@@ -1,45 +1,7 @@
-var names = ["John Smith", "Jackie Jackson", "Chris Jones", "Amanda Cullen", "Jeremy Goodwin"];
-console.log(names);
-var arrayTwo = ["Jackie Jackson",
-		"Chris Jones",
-		"Amanda Cullen",
-		"Jeremy Goodwin"];
-var arrayThree = ["John Smith",
-		"Chris Jones",
-		"Amanda Cullen",
-		"Jeremy Goodwin"];
-var arrayFour = ["John Smith",
-		"Jackie Jackson",
-		"Amanda Cullen",
-		"Jeremy Goodwin"];
-var arrayFive = ["John Smith",
-		"Jackie Jackson",
-		"Chris Jones",
-		"Jeremy Goodwin"];
-var arraySix = ["John Smith",
-		"Jackie Jacson",
-		"Chris Jones",
-		"Amanda Cullen"];
-var remove = prompt('Which name do you whish to delete');
-if(remove === 'John Smith'){
-	console.log(arrayTwo);
-}
-else if(remove === 'Jackie Jackson'){
-	console.log(arrayThree);
+var eArray = ["John Snow", "Marla Singer", "Rigby James", "Cool Joe", "Morty Rick"];
+console.log("List of Employees\:" + eArray);
+var getOuttaHere = prompt("Which one of these names would you like to remove from the list\?");
+for(i = 0; i < eArray.length; i++){
+	eArray.slice === i + "<br>";
 
 }
-else if(remove === 'Chris Jones'){
-	console.log(arrayFour);
-}
-else if(remove === 'Amanda Cullen'){
-	console.log(arrayFive);
-}
-else if(remove === 'Jeremy Goodwin'){
-	console.log(arraySix);
-}
-else{
-	console.log("you done something wrong check your spelling and puncuation");
-}
-
-
-

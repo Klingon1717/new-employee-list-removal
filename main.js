5e08d810de8c0799cb448c35a82c45e763041f60
Whitney Murray <wnksmith92@yahoo.com>
2015-12-17 12:30:24 -0600

    finished

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-var eArray = ["John Snow", "Marla Singer", "Rigby James", "Cool Joe", "Morty Rick"];
-console.log("List of Employees\:" + eArray);
-var getOuttaHere = prompt("Which one of these names would you like to remove from the list\?");
-for(i = 0; i < eArray.length; i++){
-	eArray.slice === i + "<br>";
 
+var nameArray = ['John Snow', 'Gill Bates', 'Cool Joe', 'Rigby James', 'Morty Rick'];
+//holding Employees names for later use, and adding the array of names.
+var first = "Employees:" + nameArray;
+console.log(first);
+// asking which name they would like to remove
+var userAns = prompt("Who do you wish to remove from the list\?");
+
+// We want to look through each name one at a time. 
+for(var i = 0; i < nameArray.length; i++){
+	//We checking each element in an array to see if it matches userAns
+	if(userAns === nameArray[i] ){
+		var remove = nameArray.splice(i,1);
+		//After finding the matching element to userAns, I want to use my remove var and splice that guy out of there.
+
+	}
 }
+//I want to display the new array with four names that has been spliced
+var second = "Employees:" + nameArray;
+console.log(second);
